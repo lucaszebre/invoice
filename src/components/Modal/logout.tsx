cb5ccd2ef0a0a50398000dcaf9748347b72bd691
lucaszebre <yakirisk@gmail.com>
2023-05-11 10:53:15 +0200

    delete signout function

--- src/components/Modal/logout.tsx
+++ src/components/Modal/logout.tsx
@@ -2,11 +2,23 @@ import React from 'react';
 import styles from '@/styles/logout.module.css';
 import { useInvoice } from '@/context/InvoiceContext'; // import the useInvoice hook
 import ProfileImageUpload from '../ProfileImageUpload';
-import { handleSignOut } from '@/utils/signout';
+import { auth } from '@/config/firebase'; // import auth from your firebase.ts file
+import { useRouter } from 'next/dist/client/router';
 const Logout = () => {
+    
+    const router = useRouter();
 
     const {isLight} = useInvoice();  // use the hook
 
+    const handleSignOut = async () => {
+        try {
+        await auth.signOut();
+        router.push('/connexion')
+        } catch (error) {
+        console.error('Error signing out:', error);
+        alert('Error signing out');
+        }
+    };
     return (
         <>
         <div className={`${styles.Logout} ${isLight ? styles.light : styles.dark}`}>
